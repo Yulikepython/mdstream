b519518fbc0b216c347b870b990a7fa6856e81d8
Hiroshi Nishito <admin@itc.tokyo>
2025-01-19 23:38:19 +0900

    init]規約類の追加、dompurifyによる実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 // src/App.tsx
-
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import MarkdownList from "./components/MarkdownList";
 import MarkdownDetail from "./components/MarkdownDetail";
+import PrivacyPage from "./components/PrivacyPage";
+import TermsPage from "./components/TermsPage";
 import styles from "./styles/App.module.scss";
 
 function App() {
@@ -10,7 +11,6 @@ function App() {
         <div className={styles.appContainer}>
             <Router>
                 <header className={styles.header}>
-                    {/* "MDStream" をクリックすると "/" に移動 */}
                     <h1>
                         <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
                             MDStream
@@ -22,11 +22,22 @@ function App() {
                     <Routes>
                         <Route path="/" element={<MarkdownList />} />
                         <Route path="/detail/:slug" element={<MarkdownDetail />} />
+                        <Route path="/privacy" element={<PrivacyPage />} />
+                        <Route path="/terms" element={<TermsPage />} />
                     </Routes>
                 </main>
 
                 <footer className={styles.footer}>
                     <p>&copy; 2025 MDStream. All rights reserved.</p>
+                    {/* フッターのリンクを追加 */}
+                    <nav style={{ marginTop: "0.5rem" }}>
+                        <Link to="/terms" style={{ marginRight: "1rem", color: "#fff" }}>
+                            利用規約
+                        </Link>
+                        <Link to="/privacy" style={{ color: "#fff" }}>
+                            個人情報保護
+                        </Link>
+                    </nav>
                 </footer>
             </Router>
         </div>
